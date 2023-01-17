0744d276e65e690767dbaedf66c09636bc860a4a
MODEDGES <106741581+MODEDGES@users.noreply.github.com>
2023-01-17 18:43:31 +0000

    Updated Makefiles, Lunch
    Lineage -> Spark

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_d1.mk
+    $(LOCAL_DIR)/spark_d1.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_d1-eng \
-    lineage_d1-user \
-    lineage_d1-userdebug
+    spark_d1-eng \
+    spark_d1-user \
+    spark_d1-userdebug
